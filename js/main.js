03c5a71d5a4cb2942b2f559f4a4d595f1753e57e
cyber-wf13 <o.zon77788@gmail.com>
2021-02-16 14:17:33 +0200

    finally work with arrows

--- js/main.js
+++ js/main.js
@@ -4,22 +4,22 @@ let menu = $('.menu');
 let cards = document.querySelectorAll('.block-image__card');
 let center = document.querySelectorAll('.block-image__center');
 
-headerMenuBtn.on('click', function(){
+headerMenuBtn.on('click', function () {
   menu.animate({
     'top': '-4px',
-  },500,'linear')
+  }, 500, 'linear')
 });
 
-menuBtn.on('click', function(){
+menuBtn.on('click', function () {
   menu.animate({
     'top': '-100px',
-  },500,'linear')
+  }, 500, 'linear')
 });
 
-let cardTopLeft = LeaderLine.pointAnchor(cards[0], {x: 360, y: 52.5});
-let cardBottomLeft = LeaderLine.pointAnchor(cards[1], {x: 410, y: 52.5});
+let cardTopLeft = LeaderLine.pointAnchor(cards[0], {x: cards[0].clientWidth + 5, y: 52.5});
+let cardBottomLeft = LeaderLine.pointAnchor(cards[1], {x: cards[1].clientWidth + 5, y: 52.5});
 
-new LeaderLine(LeaderLine.pointAnchor(center[0], {x: -5, y: center[0].clientHeight / 3}), cardTopLeft, {
+let propertyLineRight = {
   color: '#3EC695',
   size: 2,
   path: 'grid',
@@ -30,33 +30,9 @@ new LeaderLine(LeaderLine.pointAnchor(center[0], {x: -5, y: center[0].clientHeig
   endPlugSize: 1,
   startPlugColor: '#4EBD88',
   endPlugColor: '#4EBD88',
-}).hide('none').show('draw', {
-    duration: 800,
-  });
+}
 
-
-new LeaderLine(LeaderLine.pointAnchor(center[0], {x: -5, y: (center[0].clientHeight / 3) * 2}), cardBottomLeft, {
-  color: '#3EC695',
-  size: 2,
-  path: 'grid',
-  endSocket: 'right',
-  startPlug: 'disc',
-  endPlug: 'disc',
-  startPlugSize: 1,
-  endPlugSize: 1,
-  startPlugColor: '#4EBD88',
-  endPlugColor: '#4EBD88',
-}).hide('none').show('draw', {
-  duration: 800,
-});
-
-
-let cardTopRight = LeaderLine.pointAnchor(cards[2], {x: -5, y: 52.5});
-let cardBottomRight = LeaderLine.pointAnchor(cards[3], {x: -5, y: 52.5});
-
-
-
-new LeaderLine(LeaderLine.pointAnchor(center[0], {x: 165, y: center[0].clientHeight / 3}), cardTopRight, {
+let propertyLineLeft = {
   color: '#3EC695',
   size: 2,
   path: 'grid',
@@ -67,93 +43,48 @@ new LeaderLine(LeaderLine.pointAnchor(center[0], {x: 165, y: center[0].clientHei
   endPlugSize: 1,
   startPlugColor: '#4EBD88',
   endPlugColor: '#4EBD88',
-}).hide('none').show('draw', {
+}
+
+new LeaderLine(LeaderLine.pointAnchor(center[0], { x: -5, y: center[0].clientHeight / 3 }), cardTopLeft, propertyLineRight).hide('none').show('draw', {
   duration: 800,
 });
-new LeaderLine(LeaderLine.pointAnchor(center[0], {x: 165, y: (center[0].clientHeight / 3 * 2)}), cardBottomRight, {
-  color: '#3EC695',
-  size: 2,
-  path: 'grid',
-  endSocket: 'left',
-  startPlug: 'disc',
-  endPlug: 'disc',
-  startPlugSize: 1,
-  endPlugSize: 1,
-  startPlugColor: '#4EBD88',
-  endPlugColor: '#4EBD88',
-}).hide('none').show('draw', {
+
+new LeaderLine(LeaderLine.pointAnchor(center[0], { x: -5, y: (center[0].clientHeight / 3) * 2 }), cardBottomLeft, propertyLineRight).hide('none').show('draw', {
   duration: 800,
 });
 
 
+let cardTopRight = LeaderLine.pointAnchor(cards[2], { x: -5, y: 52.5 });
+let cardBottomRight = LeaderLine.pointAnchor(cards[3], { x: -5, y: 52.5 });
 
-
-let cardTopLeftSkill = LeaderLine.pointAnchor(cards[4], {x: 290, y: 50});
-let cardBottomLeftSkill = LeaderLine.pointAnchor(cards[5], {x: 290, y: 50});
-
-new LeaderLine(LeaderLine.pointAnchor(center[1], {x: -5, y: center[1].clientHeight / 3}), cardTopLeftSkill, {
-  color: '#3EC695',
-  size: 2,
-  path: 'grid',
-  endSocket: 'right',
-  startPlug: 'disc',
-  endPlug: 'disc',
-  startPlugSize: 1,
-  endPlugSize: 1,
-  startPlugColor: '#4EBD88',
-  endPlugColor: '#4EBD88',
-}).hide('none').show('draw', {
-    duration: 800,
-  });
-
-
-new LeaderLine(LeaderLine.pointAnchor(center[1], {x: -5, y: (center[1].clientHeight / 3) * 2}), cardBottomLeftSkill, {
-  color: '#3EC695',
-  size: 2,
-  path: 'grid',
-  endSocket: 'right',
-  startPlug: 'disc',
-  endPlug: 'disc',
-  startPlugSize: 1,
-  endPlugSize: 1,
-  startPlugColor: '#4EBD88',
-  endPlugColor: '#4EBD88',
-}).hide('none').show('draw', {
+new LeaderLine(LeaderLine.pointAnchor(center[0], { x: 165, y: center[0].clientHeight / 3 }), cardTopRight, propertyLineLeft).hide('none').show('draw', {
+  duration: 800,
+});
+new LeaderLine(LeaderLine.pointAnchor(center[0], { x: 165, y: (center[0].clientHeight / 3 * 2) }), cardBottomRight, propertyLineLeft).hide('none').show('draw', {
   duration: 800,
 });
 
 
-let cardTopRightSkill = LeaderLine.pointAnchor(cards[6], {x: -5, y: 50});
-let cardBottomRightSkill = LeaderLine.pointAnchor(cards[7], {x: -5, y: 50});
+let cardTopLeftSkill = LeaderLine.pointAnchor(cards[4], { x: cards[4].clientWidth + 5, y: 50 });
+let cardBottomLeftSkill = LeaderLine.pointAnchor(cards[5], { x: cards[5].clientWidth + 5, y: 50 });
 
-
-
-new LeaderLine(LeaderLine.pointAnchor(center[1], {x: 205, y: center[1].clientHeight / 3}), cardTopRightSkill, {
-  color: '#3EC695',
-  size: 2,
-  path: 'grid',
-  endSocket: 'left',
-  startPlug: 'disc',
-  endPlug: 'disc',
-  startPlugSize: 1,
-  endPlugSize: 1,
-  startPlugColor: '#4EBD88',
-  endPlugColor: '#4EBD88',
-}).hide('none').show('draw', {
+new LeaderLine(LeaderLine.pointAnchor(center[1], { x: -5, y: center[1].clientHeight / 3 }), cardTopLeftSkill, propertyLineRight).hide('none').show('draw', {
   duration: 800,
 });
-new LeaderLine(LeaderLine.pointAnchor(center[1], {x: 205, y: (center[1].clientHeight / 3 * 2)}), cardBottomRightSkill, {
-  color: '#3EC695',
-  size: 2,
-  path: 'grid',
-  endSocket: 'left',
-  startPlug: 'disc',
-  endPlug: 'disc',
-  startPlugSize: 1,
-  endPlugSize: 1,
-  startPlugColor: '#4EBD88',
-  endPlugColor: '#4EBD88',
-}).hide('none').show('draw', {
+
+
+new LeaderLine(LeaderLine.pointAnchor(center[1], { x: -5, y: (center[1].clientHeight / 3) * 2 }), cardBottomLeftSkill, propertyLineRight).hide('none').show('draw', {
+  duration: 800,
+});
+
+
+let cardTopRightSkill = LeaderLine.pointAnchor(cards[6], { x: -5, y: 50 });
+let cardBottomRightSkill = LeaderLine.pointAnchor(cards[7], { x: -5, y: 50 });
+
+new LeaderLine(LeaderLine.pointAnchor(center[1], { x: 205, y: center[1].clientHeight / 3 }), cardTopRightSkill, propertyLineLeft).hide('none').show('draw', {
+  duration: 800,
+});
+new LeaderLine(LeaderLine.pointAnchor(center[1], { x: 205, y: (center[1].clientHeight / 3 * 2) }), cardBottomRightSkill, propertyLineLeft).hide('none').show('draw', {
   duration: 800,
 });
 
